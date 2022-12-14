755ff7057fad6aa77891280d6e89737a890c993a
marcensie <aquila19908@gmail.com>
2022-12-14 13:16:41 +0200

    email test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace lab4_idweb.Controllers
                _logger = logger;
           }
 
-          public IActionResult Index()
+          public IActionResult Index INdex()
           {
                return View();
           }
